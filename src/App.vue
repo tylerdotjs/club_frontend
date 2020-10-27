f78926c8c99ec545e52ba86efd19f3ce40e615b1
tylerdotjs <tylerdotjs@gmail.com>
2020-10-27 12:58:09 +0000

    Implemented simple webpage navigation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <navigation />
   <router-view/>
 </template>
-
+<script>
+import navigation from '@/components/nav.vue'
+export default {
+    components: {
+        navigation
+    }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -14,17 +18,7 @@
   text-align: center;
   color: #2c3e50;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+body {
+    margin: 0px;
 }
 </style>
---
+++ src/components/nav.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="nav">
+        <div class="nav_logo"><router-link to="/">[Logo]</router-link></div>
+        <div class="nav_menu">
+            <router-link to="/">home</router-link>
+            <router-link to="/about">test</router-link>
+        </div>
+        <div class="nav_user"><h>[User Stuff]</h></div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="css">
+    .nav {
+        display: flex;
+        width: 100%;
+        height: 50px;
+        background: lightgrey;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .nav_logo {
+        width: 10%;
+    }
+    .nav_menu {
+        display: flex;
+    }
+    .nav_menu > a {
+        color: black;
+        text-decoration: none;
+        margin: 10px;
+    }
+    .nav_menu > a:hover {
+        color: grey;
+    }
+    .nav_menu > .router-link-exact-active {
+        text-decoration: underline;
+    }
+    .nav_user {
+        width: 10%;
+    }
+</style>
